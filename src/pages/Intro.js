a091489168c028854364976fe23a680a762f3a66
Ragnhild Amdam <ragamdam@gmail.com>
2019-03-19 12:15:13 +0100

    Update Intro.js
    Endret tekst på forsiden til veiviseren.

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -13,23 +13,18 @@ function Intro({ close, data }) {
       <IntroMain>
         <Primitives.Heading.H1>Finn ut om du kan erklære ansvar!</Primitives.Heading.H1>
         <Primitives.Paragraphs.P>
-          Dette er en veiviser for deg som vil finne ut om firmaet ditt er kvalifisert til å erklære
-          ansvar i et byggeprosjekt. Det vil si at firmaet ditt kan ta ansvar for de delene av
-          arbeidet som dere vil gjøre, og at dere står ansvarlig for at arbeidet utføres riktig. Vi
-          hjelper deg også å lage en erklæring for ansvarsrett som du kan skrive ut, signere og
-          sende til ansvarlig søker.
+          Denne veiviseren hjelper deg å finne ut om firmaet ditt kan erklære ansvar i et byggeprosjekt. Dette krever at den faglige ledelsen som skal delta i prosjektet, må ha lang nok relevant utdanning og lang nok relevant arbeidserfaring. Er firmaet ditt kvalifisert, får du hjelp til å lage en erklæring for ansvarsrett som du kan skrive ut, signere og sende til ansvarlig søker. Mangler firmaet kompetanse, får du hjelp til å finne ut om du kan søke kommunen om ansvarsrett. 
         </Primitives.Paragraphs.P>
         <Primitives.Paragraphs.P>
-          Du får kun velge ett ansvarsområde med én tiltaksklasse og én funksjon for hver gang du
-          gjennomfører denne veiviseren.
+        Du må selv vurdere om utdanningen og erfaringen til den faglige ledelsen er relevant. Du kan velge ett ansvarsområde med én tiltaksklasse og én funksjon for hver gang du tar veiviseren. 
         </Primitives.Paragraphs.P>
         <section>
           <div>
             <Primitives.Heading.H2 small>Før du begynner må du vite:</Primitives.Heading.H2>
             <ol>
               <li>Organisasjonsnummeret til firmaet ditt</li>
-              <li>Utdanningen og erfaringen til faglig ledelse i firmaet ditt</li>
               <li>Hva firmaet ditt skal ta ansvar for</li>
+              <li>Hvor lang relevant utdanning og relevant erfaring faglig ledelse i firmaet ditt har</li>
               <li>Hvilket firma som er ansvarlig søker </li>
             </ol>
           </div>
@@ -39,16 +34,12 @@ function Intro({ close, data }) {
             </Primitives.Figure>
           </div>
         </section>
-        <Primitives.Heading.H2 small>Usikker?</Primitives.Heading.H2>
+        <Primitives.Heading.H2 small>Start nå og fullfør senere</Primitives.Heading.H2>
         <Primitives.Paragraphs.P>
-          Snakk med ansvarlig søker eller oppdragsgiver.
+          Trenger du å gjøre avklaringer med for eksempel oppdragsgiver eller ansvarlig søker underveis? Nettleseren husker hvor du stopper, så du kan ta en pause og fortsette senere.
+          
         </Primitives.Paragraphs.P>
-        <Primitives.Heading.H2 small>Når veiviseren er fullført</Primitives.Heading.H2>
-        <Primitives.Paragraphs.P>
-          Når du har fullført veiviseren får du vite om du kan erklære ansvar eller ikke. Du kan
-          skrive ut og signere erklæringen, slik at du kan sende den til ansvarlig søker.
-        </Primitives.Paragraphs.P>
-        <Primitives.Button.MainButton type="button" onClick={() => close()}>
+               <Primitives.Button.MainButton type="button" onClick={() => close()}>
           Start veiviseren
         </Primitives.Button.MainButton>
       </IntroMain>
